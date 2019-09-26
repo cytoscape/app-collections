a0493babadd5c5fa5b881214ce780e21912b206f
Alexander Pico <apico@gladstone.ucsf.edu>
2019-09-25 17:35:36 -0700

    migrated and updated enrichmentmap collection

---
+++ enrichmentmap-collection/src/main/java/org/cytoscape/enrichmentmapcollection/internal/CyActivator.java
@@ -0,0 +1,14 @@
+package org.cytoscape.enrichmentmapcollection.internal;
+
+import java.util.Properties;
+
+import org.cytoscape.service.util.AbstractCyActivator;
+import org.osgi.framework.BundleContext;
+
+public class CyActivator extends AbstractCyActivator {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+	}
+
+}
